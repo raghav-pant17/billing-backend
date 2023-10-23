33684c5c1bcd9ef43585e478e74cd5aa83c7c417
Rohan <Rohan230901@gmail.com>
2023-10-23 13:01:00 +0530

    final minus one

--- src/main/java/com/billingproj/billingapp/BillingappApplication.java
+++ src/main/java/com/billingproj/billingapp/BillingappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BillingappApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BillingappApplication.class, args);
 	}
